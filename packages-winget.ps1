60db79167e6d533368f01bcb7779f338c635835c
dkadev <23488064+dkadev@users.noreply.github.com>
2024-11-27 18:48:48 +0100

    Refactor package installation scripts to improve user interaction and streamline installation process

--- packages-winget.ps1
+++ packages-winget.ps1
@@ -5,14 +5,9 @@ winget install --id=7zip.7zip -e
 winget install --id=Git.Git -e
 winget install --id=Microsoft.WindowsTerminal -e
 winget install --id=VideoLAN.VLC -e
-winget install --id=Microsoft.PowerToys -e
 
 # PASSWORD MANAGER
 winget install --id=KeePassXCTeam.KeePassXC  -e
-# winget install --id=DominikReichl.KeePass -e
-
-# MAIL
-winget install --id=Mozilla.Thunderbird -e
 
 # EDITOR  
 # winget install --id=Microsoft.VisualStudioCode -e
@@ -20,20 +15,10 @@ winget install --id=VSCodium.VSCodium -e
 
 # SECURITY
 winget install --id=GnuPG.Gpg4win -e
-# winget install --id=NordVPN.NordVPN -e
-# winget install --id=Twilio.Authy -e
 
 # DISK SPACE ANALYZER
 winget install --id=AntibodySoftware.WizTree -e
-# winget install --id=WinDirStat.WinDirStat -e
 
 # BROWSER
 winget install --id=Mozilla.Firefox -e
-# winget install --id=Mozilla.Firefox.DeveloperEdition -e
-# winget install --id=LibreWolf.LibreWolf -e
 winget install --id=Hibbiki.Chromium -e
-
-# EXTRA
-# winget install --id=TimKosse.FileZilla.Client -e
-# winget install --id=qBittorrent.qBittorrent -e
-winget install --id=Oracle.VirtualBox -e
--- packages.ps1
+++ packages.ps1
@@ -1,3 +1,3 @@
 PowerShell -NoExit .\packages-winget.ps1
+PowerShell -NoExit .\packages-scoop.ps1
 # PowerShell -NoExit .\packages-choco.ps1
-# PowerShell -NoExit .\packages-scoop.ps1
